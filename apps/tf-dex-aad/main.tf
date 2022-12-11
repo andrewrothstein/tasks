4ba171b09eaca929d6cd0729799980680bc045af
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-12-11 13:19:57 -0500

    tf-dex-app:delete, generate an AAD app passwd

--- apps/tf-dex-aad/main.tf
+++ apps/tf-dex-aad/main.tf
@@ -1,3 +1,16 @@
 resource "azuread_application" "dex-dev" {
   display_name = "dex-dev"
+
+}
+
+resource "time_rotating" "dex-dev-pwd-ttl" {
+  rotation_days = 90
+}
+
+resource "azuread_application_password" "dex-dev-pwd" {
+  display_name = "dex-dev-pwd"
+  application_object_id = azuread_application.dex-dev.object_id
+  rotate_when_changed = {
+    rotation = time_rotating.dex-dev-pwd-ttl.id
+  }
 }
